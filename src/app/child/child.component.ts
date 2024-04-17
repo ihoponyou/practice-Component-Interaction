363afb7aa003cdc2a17d187ca37aca1d465d9f24
kotala-sudo <79818586+kotala-sudo@users.noreply.github.com>
2024-04-17 10:56:15 -0500

    Update child.component.ts
    Fixed the selector error for childcomponent

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-child',
+  selector: 'app-childcomponent',
   standalone: true,
   imports: [],
   templateUrl: './child.component.html',
