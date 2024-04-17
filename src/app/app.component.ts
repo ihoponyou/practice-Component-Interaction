f0f64f1dce128254db1f7a8c41a2be6cb3fdc06e
noahmagcalas <noah.magcalas@gmail.com>
2024-04-17 12:03:41 -0500

    add input/output bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './child/child.component';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ChildComponent],
+  imports: [RouterOutlet, ChildComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  pText: string = "";
+  childMessage: string = "";
+
+  readMessage(msg: string) {
+    this.childMessage = msg;
+  }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-childcomponent',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './child.component.html',
   styleUrl: './child.component.css'
 })
 export class ChildComponent {
+  @Input() receivedText: string = "";
+  message: string = "";
+  @Output() sentMessage: EventEmitter<string> = new EventEmitter();
 
+  sendMessage(msg: string) {
+    this.sentMessage.emit(msg);
+  }
 }
